74b25c74fd1c17a127ca9ffee4589c9f20895fc1
Jason Reed <jcreed@gmail.com>
2025-06-30 08:15:18 -0400

    Add comment

--- src/search.rs
+++ src/search.rs
@@ -14,6 +14,13 @@ pub struct State {
 }
 
 pub fn quat_in_in_patch(env: &Env, quat: &Quat<Interval<Rational>>) -> bool {
+    //// To get this to work, I need to figure out how intervals behave with normalization.
+    //// The good news is that all the quaternions I'm dealing with are going to have
+    //// squared norm strictly away from zero. They're all 1 + aî + bĵ + ck̂. So even if
+    //// a, b, c are intervals, maybe even containing zero, the inverse squared norm
+    //// is going to be ≤ 1.
+    // let rotated_vertices = crate::geom::rotate_vertices(quat, &env.vertices);
+
     for (i, face) in env.faces.iter().enumerate() {
         //
     }
